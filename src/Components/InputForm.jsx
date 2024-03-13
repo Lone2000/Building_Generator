5065468031ad023f236533e8db7d9edea040c80b
Irfan <lone0059@gmail.com>
2024-03-13 17:48:14 +0500

    feat: Floor Input Implementation

--- src/Components/InputForm.jsx
+++ src/Components/InputForm.jsx
@@ -1,8 +1,29 @@
+import { useState } from "react";
+
+import styles from "./InputForm.module.css";
+
 function InputForm() {
+  const [floor, setFloor] = useState();
+
+  const handleFloorChange = (e) => {
+    setFloor(e.target.value);
+  };
+
   return (
     <>
       <form action="">
-        <input type="checkbox" name="" id="" />
+        <div className={styles.slidecontainer}>
+          <label htmlFor="build_floors">{floor} Floors</label>
+          <input
+            type="range"
+            min="2"
+            max="10"
+            name="build_floors"
+            id="build_floors"
+            className={styles.slider}
+            onChange={handleFloorChange}
+          />
+        </div>
       </form>
     </>
   );
