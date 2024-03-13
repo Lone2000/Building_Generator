64f0cb58827f4dd1f3fa07d4ee13d649ee8d98e7
Irfan <lone0059@gmail.com>
2024-03-13 23:50:28 +0500

    feat: Implementation of Creating Buildings Using Shape & Floor User Input

--- src/Components/Generator.jsx
+++ src/Components/Generator.jsx
@@ -1,7 +1,27 @@
-function Generator() {
+import styles from "./Generator.module.css";
+
+function Generator({ floor, shape }) {
+  const calculateWidth = (shape) => {
+    return shape === "square" ? "500px" : "250px";
+  };
+  console.log(typeof floor);
+  const calculateHeight = (floor) => {
+    return String(floor * 50) + "px";
+  };
+
+  const style = {
+    width: calculateWidth(shape),
+    height: calculateHeight(parseInt(floor)),
+    backgroundColor: "#aaa",
+    position: "relative",
+  };
+
   return (
     <>
-      <h2>Put Input form here</h2>
+      <h2>Building View</h2>
+      <div className={styles.building_container}>
+        <div style={style}></div>
+      </div>
     </>
   );
 }
